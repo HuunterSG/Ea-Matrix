117cee635d94def82dc73749bd4108629b43f970
HuunterSG <agustinjperez.r@gmail.com>
2026-01-10 16:41:19 -0300

    Upgrade

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
-// Menú hamburguesa
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
 });
-document.querySelectorAll('.nav-link').forEach(link => link.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-}));
+
+// Cierra menú al clickear un link (buena práctica)
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    });
+});
 
 // Header scrolled
 window.addEventListener('scroll', () => {
